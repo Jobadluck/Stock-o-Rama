5141f506538ecfa2b04317e4e901f830ec9507ee
Pons Jean-Baptiste <jean-baptiste.pons@dedalus.com>
2024-02-16 10:50:18 +0100

    feat: add page.server file at root
    Signed-off-by: Pons Jean-Baptiste <jean-baptiste.pons@dedalus.com>

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,5 @@
+let NBR_CALL = 0
+
+export const load = () => {
+    return { callNbr: NBR_CALL++ }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+    export let data
+    const { callNbr } = { callNbr: data.callNbr }
+</script>
+
 <h1>Welcome to Stock-o-Rama</h1>
+<p>Page d'accueil</p>
+<span>Nombre d'appel de la fonction load coté serveur: {callNbr}</span>
